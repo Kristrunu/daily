5ead2a07a0dd4f24d3b092a62447e60ca810c31d
Kristrún Úlfarsdóttir <kristrunulfarsdottir@Kristruns-MacBook-Pro.local>
2022-01-27 15:13:23 +0000

    styling components

--- frontend/src/components/EntryList/EntryList.js
+++ frontend/src/components/EntryList/EntryList.js
@@ -2,14 +2,13 @@ import "./EntryList.css";
 import Entry from "../Entry/Entry";
 import Button from "../Button/Button";
 
-
 const EntryList = ({ entryList }) => {
   return (
       <>
       <h1 className="logo">DAILY</h1>
       <div className="btns">
       <Button btnClass="btn-dark plus-icon" text="New Entry" />
-      <Button btnClass="btn-light user-icon" text="Username" />
+      <Button btnClass="btn-light user-icon" text="Log Out" />
       </div>
       <div className="entry-list">
         {entryList.map((entryItem, index) => {
