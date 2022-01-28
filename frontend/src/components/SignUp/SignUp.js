15d58171bac8f32474604fd013d36c34c7d36b4c
Kristrun Ulfarsdottir <kristrunu@gmail.com>
2022-01-28 13:36:35 +0000

    add signup component

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -1,7 +1,32 @@
 import "./SignUp.css";
+import Button from "../Button/Button";
 
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <form className="signup-form">
+        <h2 className="signup-title">Sign Up</h2>
+        
+         <div className="input-form">
+          <label htmlFor="email"> Email Address </label>
+          <input type="email" name="email" id="email"/>
+        </div>
+
+        <div className="input-form">
+          <label htmlFor="password"> Password </label>
+          <input type="password" name="password" id="password"/>
+        </div>
+
+        <div className="info-links">
+          <a href="#" className="forgot-pass">Forgot Password</a>
+          <a href="#" className="sign-up">No account? Sign up here!</a>
+        </div>
+        
+        <div className="login-btn">
+          <Button btnClass="btn-dark" text="Sign Up" />
+        </div>
+
+    </form>
+  )
 };
 
 export default SignUp;
