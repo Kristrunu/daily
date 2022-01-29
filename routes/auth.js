b89c5722c43e968dfac147efc292c4f595276d2e
Tinnahalls <tinnahalls@gmail.com>
2022-01-29 13:43:21 +0000

    set up logout, edit entry,

--- routes/auth.js
+++ routes/auth.js
@@ -44,6 +44,19 @@ try {
   // save the user to the database 
   const savedUser = await newUser.save(); 
 
+  const payload = { userId: savedUser._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    res.cookie("access-token", token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true, 
+      secure: process.env.NODE_ENV === "production"
+    });
+
+
   const userToReturn = { ...savedUser._doc }
   delete userToReturn.password; 
 
@@ -58,7 +71,7 @@ try {
 })
 
 
-// @route   POST /api/auth/loggin
+// @route   POST /api/auth/login
 // @desc    Login user and return a access token
 // @access  Public 
 
@@ -104,7 +117,6 @@ router.post("/login", async (req,res) => {
   }
 });
 
-
 // @route   GET /api/auth/current
 // @desc    Currently Authed User
 // @access  Private
@@ -116,6 +128,23 @@ router.get("/current", requiresAuth, (req,res) => {
 
   return res.json(req.user);
 });
+
+// @route   PUT /api/auth/logout
+// @desc    Logout user and clear the cookie 
+// @access  Private
+
+router.put("/logout", requiresAuth, async(req,res) => {
+  try {
+    res.clearCookie("access-token");
+    return res.json( {success: true});
+
+  } catch(err) {
+    console.log(err)
+    return res.status(500).send(err.message);
+  }
+})
+
+
 
 
 module.exports = router; 
