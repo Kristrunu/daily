76eed315ccf8ce5c13f1f84f5710c6aee4c5532e
Kristrun Ulfarsdottir <kristrunu@gmail.com>
2022-01-29 14:44:55 +0000

    fix header and icons

--- frontend/src/components/EntryList/EntryList.js
+++ frontend/src/components/EntryList/EntryList.js
@@ -9,7 +9,7 @@ const EntryList = ({ entryList }) => {
         <h1 className="daily-h1">DAILY</h1>
         <div className="btns">
         <Button btnClass="btn-dark btn-entry icon-plus-icon" text="New Entry" />
-        <Button btnClass="btn-light icon-user-icon" text="Log Out" />
+        <Button btnClass="btn-light icon-logout-icon" text="Log Out" />
         </div>
     </div>
       <div className="entry-list">
