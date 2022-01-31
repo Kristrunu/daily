c96ad609d7cfd02479e8760c832a8ec07aa2aaa6
Tinnahalls <tinnahalls@gmail.com>
2022-01-31 16:31:22 +0000

    created entryview

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useGlobalContext } from "../../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
 import ToDoCard from "../ToDoCard/ToDoCard";
+import EmptyState from "../EmptyState/EmptyState"
 import './Dashboard.css';
 
 const Dashboard = () => {
@@ -16,22 +17,28 @@ const Dashboard = () => {
 
     return (
         <div className="dashboard">
-            <div className="todos">
-                {incompleteToDos.map((toDo) => (
-                    <ToDoCard toDo={toDo} key={toDo._id} />
-                ))}
-            </div>
-
-            {completeToDos.length > 0 && (
+            {completeToDos.length === 0 && incompleteToDos.length === 0 ? (
+               <EmptyState /> 
+            ):(
+                <>
                 <div className="todos">
-                    <h2 className="todos__title">Complete Todo's</h2>
-                    {completeToDos.map((toDo) => (
+                    {incompleteToDos.map((toDo) => (
                         <ToDoCard toDo={toDo} key={toDo._id} />
                     ))}
                 </div>
-            )}
-            
+
+                {completeToDos.length > 0 && (
+                    <div className="todos">
+                        <h2 className="todos__title">Complete Todo's</h2>
+                        {completeToDos.map((toDo) => (
+                            <ToDoCard toDo={toDo} key={toDo._id} />
+                        ))}
+                    </div>
+                )}
+                </> 
+            )};
         </div>
+    
     );
 };
 
--- frontend/src/components/Entry/Entry.js
+++ frontend/src/components/Entry/Entry.js
@@ -1,15 +1,33 @@
+import React from "react";
 import "./Entry.css";
 
 const Entry = ({ entry }) => {
+  const[focusEntry, setFocusEntry] = React.useState("");
+  const[eveningEntry, setEveningEntry] = React.useState("");
   return (
     <div className="entry">
       <div className="entry__date">
         <h1>{entry.date}</h1>
       </div>
+
       <div className="entry__content">
-        <div className="entry__title">{entry.title}</div>
-        <div className="entry__text">{entry.text}</div>
+        <div className="entry__text">
+          <label>Today I will focus on.. </label>
+          <input
+          type="text"
+          value={focusEntry}
+          onChange={(e) => setFocusEntry(e.target.value)} />
+           </div>
+
+        <div className="entry__text">
+          <label>Evening thoughts</label>
+          <input
+          type="text"
+          value={eveningEntry}
+          onChange={(e) => setEveningEntry(e.target.value)} />
+        </div>
       </div>
+
     </div>
   );
 };
