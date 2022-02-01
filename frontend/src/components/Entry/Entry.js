c86066eea0ddf3990d4622fb30a0cf369ec1bb18
Tinnahalls <tinnahalls@gmail.com>
2022-02-01 09:25:37 +0000

    work on entry, push for pull

--- frontend/src/components/Entry/Entry.js
+++ frontend/src/components/Entry/Entry.js
@@ -2,32 +2,38 @@ import React from "react";
 import "./Entry.css";
 
 const Entry = ({ entry }) => {
-  const[focusEntry, setFocusEntry] = React.useState("");
-  const[eveningEntry, setEveningEntry] = React.useState("");
+  const [focusEntry, setFocusEntry] = React.useState("");
+  const [journalEntry, setJournalEntry] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [errors, setErrors] = React.useState({});
+
   return (
     <div className="entry">
       <div className="entry__date">
         <h1>{entry.date}</h1>
       </div>
 
-      <div className="entry__content">
-        <div className="entry__text">
-          <label>Today I will focus on.. </label>
-          <input
-          type="text"
-          value={focusEntry}
-          onChange={(e) => setFocusEntry(e.target.value)} />
-           </div>
+      <form onSubmit={onSubmit}>
+        <div className="entry__content">
+          <div className="entry__text">
+            <label>Today I will focus on.. </label>
+            <input
+              type="text"
+              value={focusEntry}
+              onChange={(e) => setFocusEntry(e.target.value)}
+            />
+          </div>
 
-        <div className="entry__text">
-          <label>Evening thoughts</label>
-          <input
-          type="text"
-          value={eveningEntry}
-          onChange={(e) => setEveningEntry(e.target.value)} />
+          <div className="entry__text">
+            <label>Evening thoughts</label>
+            <input
+              type="text"
+              value={journalEntry}
+              onChange={(e) => setJournalEntry(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
-
+      </form>
     </div>
   );
 };
