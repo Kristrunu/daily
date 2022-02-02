08acecaa608a67c6dd45abeda83254c3e2d5e41d
Tinnahalls <tinnahalls@gmail.com>
2022-02-02 15:44:46 +0000

    added styles to entry

--- validation/entryValidation.js
+++ validation/entryValidation.js
@@ -6,8 +6,8 @@ const validateEntryInput = data => {
 
   if(isEmpty(data.content)) {
     errors.content = "Content field can not be empty";
-  } else if (!Validator.isLength(data.content, {min:1, max: 300})) {
-    errors.content = 'Content field must be between 1 and 300 characters';
+  } else if (!Validator.isLength(data.content, {min:1, max: 1500})) {
+    errors.content = 'Content field must be between 1 and 1500 characters';
   }
 
   return {
