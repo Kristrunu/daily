86d2c6b42bc65d1939476ff162e8d9d615364cb9
Tinnahalls <tinnahalls@gmail.com>
2022-02-03 10:10:54 +0000

    changed font in greeting

--- frontend/src/components/EmptyState/EmptyState.js
+++ frontend/src/components/EmptyState/EmptyState.js
@@ -5,7 +5,8 @@ const EmptyState = ({ userName }) => {
   return (
     <div className="empty-state">
         <div className="empty-headline">
-            <h1 className="empty-h1">Hey {userName},<br/> No Journal Entries Yet</h1>
+          <p className="empty-h2"> Hey {userName},</p>
+            <h1 className="empty-h1">  No Journal Entries Yet</h1>
             <p className="empty-h2">Create Your First Memory Today!</p>
         </div>
         <EmptyImg className="empty-svg" />
